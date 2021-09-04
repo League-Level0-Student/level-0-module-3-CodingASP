f1bd5e7b995023447e5c9e22a9260d9159cc350d
CodingASP <78342170+CodingASP@users.noreply.github.com>
2021-09-04 14:21:33 -0700

    9/4/21

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -22,8 +22,17 @@ public class RobotColorChooser {
 		//3. Set the pen width to 10
 		rob.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
-		JOptionPane.showInputDialog(null, "What color would you like the pen to be; Red, Blue, or Green?");
+		String color = JOptionPane.showInputDialog(null, "What color would you like the pen to be; Red, Blue, or Green?");
 		//5. Use an if/else statement to set the pen color that the user requested
+		if (color.equalsIgnoreCase("Red")) {
+			rob.setPenColor(Color.red);
+		}
+		if (color.equalsIgnoreCase("Blue")) {
+			rob.setPenColor(Color.blue);
+		}
+		if (color.equalsIgnoreCase("Green")) {
+			rob.setPenColor(Color.green);
+		}
         //6. If the user doesn't enter anything, choose a random color
 		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
