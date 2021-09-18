981bc17fc642445c653ab71fc1330e63d43c9aa7
CodingASP <78342170+CodingASP@users.noreply.github.com>
2021-09-18 16:54:53 -0700

    9/18

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -10,36 +10,36 @@ public class RobotColorChooser {
 		//1. Create a new Robot
 		Robot rob = new Robot();
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-		
-		int whole = 1;
-		while (whole <100000) {
 		rob.penDown();
 		rob.setSpeed(10);
-		int count = 4;
-		while ( count < 20 ) {
-   		 rob.move(40);
-   		 rob.turn(90);
-   		 count += 4;
-   	 }
-		
-		//3. Set the pen width to 10
 		rob.setPenWidth(10);
-		//4. Ask the user what color pen they would like the robot to draw with
-		String color = JOptionPane.showInputDialog(null, "What color would you like the pen to be; Red, Blue, or Green?");
-		//5. Use an if/else statement to set the pen color that the user requested
-		if (color.equalsIgnoreCase("Red")) {
-			rob.setPenColor(Color.red);
-		}
-		if (color.equalsIgnoreCase("Blue")) {
-			rob.setPenColor(Color.blue);
-		}
-		if (color.equalsIgnoreCase("Green")) {
-			rob.setPenColor(Color.green);
-		}
-        //6. If the user doesn't enter anything, choose a random color
+
+		while (true) {
+			
+			int count = 1;
+			while ( count < 5 ) {
+				rob.move(40);
+				rob.turn(90);
+				count += 1;
+			}
 		
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
+			//3. Set the pen width to 10
+			//4. Ask the user what color pen they would like the robot to draw with
+			String color = JOptionPane.showInputDialog(null, "What color would you like the pen to be; Red, Blue, or Green?");
+			//5. Use an if/else statement to set the pen color that the user requested
+			if (color.equalsIgnoreCase("Red")) {
+				rob.setPenColor(Color.red);
+			}
+			if (color.equalsIgnoreCase("Blue")) {
+				rob.setPenColor(Color.blue);
+			}
+			if (color.equalsIgnoreCase("Green")) {
+				rob.setPenColor(Color.green);
+			}
+	        //6. If the user doesn't enter anything, choose a random color
+			
+	        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+			
 		}
 		
 
